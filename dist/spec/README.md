138d0786926a1d056d21bd7d01a1e64e9f78ec57
tbxark <tbxark@outlook.com>
2024-11-26 15:33:40 +0800

    chore: 只保留latest版json spec

--- dist/spec/README.md
+++
@@ -1,3 +0,0 @@
-# telegram-bot-api-spec
-
-This is a specification for the Telegram Bot API. It is generated from the official Telegram Bot API documentation.
--- internal/generator/spec/spec.go
+++ internal/generator/spec/spec.go
@@ -2,21 +2,16 @@ package spec
 
 import (
 	"encoding/json"
-	"github.com/TBXark/telegram-bot-api-types/internal/scrape"
 	"os"
 	"path/filepath"
+
+	"github.com/TBXark/telegram-bot-api-types/internal/scrape"
 )
 
 func RenderSpec(resp *scrape.APIResponse, dir string) error {
 	latestPath := filepath.Join(dir, "spec", "latest.json")
-	versionPath := filepath.Join(dir, "spec", resp.Version+".json")
 
 	latest, err := os.Create(latestPath)
-	if err != nil {
-		return err
-	}
-
-	version, err := os.Create(versionPath)
 	if err != nil {
 		return err
 	}
@@ -27,11 +22,6 @@ func RenderSpec(resp *scrape.APIResponse, dir string) error {
 	}
 
 	_, err = latest.Write(bytes)
-	if err != nil {
-		return err
-	}
-
-	_, err = version.Write(bytes)
 	if err != nil {
 		return err
 	}
